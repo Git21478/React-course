69285e0756ed7009d8ed32806ac2a1702b848cd1
Git21478 <ViktorWinn@mail.ru>
2022-11-06 10:00:42 +0300

    18

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import s from './Profile.module.css'
 const Profile = () => {
     return (
         
-        <div className={s.content}> 
+        <div> 
             <div>
                 <img src="https://www.industrialempathy.com/img/remote/ZiClJf-640w.avif"/>
              </div>
