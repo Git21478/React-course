71d76b9a912dece0c7078ec508c08416d15576fe
Git21478 <ViktorWinn@mail.ru>
2022-11-08 12:33:45 +0300

    31

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,11 +1,18 @@
-import s from './MyPosts.module.css'
+import React from 'react';
+import s from './MyPosts.module.css';
 import Post from './Post/Post';
+
 
 const MyPosts = (props) => {
 
-   
-
     let postElements = props.posts.map(p => <Post message={p.message} likes={p.likes}/>)
+
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value
+        alert(text)
+    }
 
     return (
         
@@ -14,9 +21,9 @@ const MyPosts = (props) => {
 
                 <div>
                     <div>
-                        <textarea></textarea>
+                        <textarea ref={newPostElement}></textarea>
                     </div>
-                        <button>Add post</button>
+                        <button onClick={addPost}>Add post</button>
                 </div>
 
                 <div className={s.posts}>
